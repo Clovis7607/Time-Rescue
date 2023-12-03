aa987c9e19a8870aa51916cb3f1e6d5b10263a1e
Clovis <fjig5116@gmail.com>
2023-12-03 14:46:57 +0800

    Initial Commit

---
+++ Time Rescue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Time Rescue
+//
+//  Created by 吳謹安 on 2023/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Time Rescue/Time_RescueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_RescueApp.swift
+//  Time Rescue
+//
+//  Created by 吳謹安 on 2023/12/3.
+//
+
+import SwiftUI
+
+@main
+struct Time_RescueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
